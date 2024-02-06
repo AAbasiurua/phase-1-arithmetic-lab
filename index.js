c19b62aacea970d7a0c5a03525f05eba9ee1e86f
AniekanabasiAbasiurua <aniekanabasi.abasiurua@gmail.com>
2024-02-06 00:37:23 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
 
-// Write your code here
+let random = Math.ceil(Math.random()) + 1;
+console.log(random);
+
+let num3 = 22; 
+let num4 = 6;
+let mod = num3 % num4;
+    
+let max = Math.max(5, 10, 15, 20);
+ 
